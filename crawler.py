275ceeb4a6b2c157605d58c53fb7e6e86f252419
yh <yh@yhs-MacBook-Air.local>
2019-12-05 18:00:54 +0800

    add in functionality of crawling two bet types

--- crawler.py
+++ crawler.py
@@ -5,10 +5,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 def set_chrome_options(headless_mode=False):
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
@@ -21,13 +18,29 @@ def set_chrome_options(headless_mode=False):
 
     return options
 
+def wait_till_element_loads(driver,xpath_query):
+    from selenium.common.exceptions import TimeoutException
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    try:
+        delay = 5 #seconds
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, xpath_query)))
+    except TimeoutException:
+        print("page took too long to load")
+
+
 def crawl_individual_page(driver,page_link):
 
     valid_bet_types = ["1X2","Total Goals Over/Under 2.5","Total Goals Over/Under 3.5"]
 
-    driver.get(page_link)
-    # TODO: add in custom check if element exists instead of sleeping
-    time.sleep(5)
+    if driver.current_url != page_link:
+        driver.get(page_link)
+
+    wait_till_element_loads(driver,'//div[@class="event-markets"]')
+
+    fixture_title = driver.find_element_by_xpath('//span[@class="event-header__event-name"]/span').text.strip()
+    fixture_date = driver.find_element_by_xpath('//span[@class="event-header__start-time"]').text.strip()
 
     bet_types_list = driver.find_elements_by_xpath('//div[@class="event-markets__market js-collapsable"]')
 
@@ -36,8 +49,26 @@ def crawl_individual_page(driver,page_link):
         
         # only crawl valid bet types
         if bet_type_string in valid_bet_types:
-            print('doing crawling for', bet_type_string)
+            bet_row = bet_type.find_element_by_xpath('.//div[@class="event-market__outcome-row"]')
+            odds = bet_row.find_elements_by_xpath('.//span[@class="button-outcome__price"]')
 
+            if bet_type_string == "1X2":
+                home_odds = odds[0].text
+                draw_odds = odds[1].text
+                away_odds = odds[2].text
+
+            elif bet_type_string == "Total Goals Over/Under 2.5" or bet_type_string == "Total Goals Over/Under 3.5":
+                over_odds = odds[0].text
+                under_odds = odds[1].text
+
+    print(fixture_title)
+    print(fixture_date)
+    print(home_odds)
+    print(draw_odds)
+    print(away_odds)
+    print(over_odds)
+    print(under_odds)
+                
 
 if __name__ == "__main__":
 
@@ -66,7 +97,10 @@ if __name__ == "__main__":
         driver_path = "./chromedriver"
         driver = webdriver.Chrome(driver_path, options=set_chrome_options())
 
-    web_url = "https://online.singaporepools.com/en/sports/competition/36/football/england/english-premier"
+    driver.maximize_window()
+
+    # web_url = "https://online.singaporepools.com/en/sports/competition/36/football/england/english-premier"
+    web_url = "https://online.singaporepools.com/en/sports/event-details/30036/football/england/english-premier/sheffield-utd-vs-newcastle"
 
     if driver.current_url != web_url:
         driver.get(web_url)
@@ -78,22 +112,16 @@ if __name__ == "__main__":
     except NoAlertPresentException:
         print("No alert appeared")
 
-    # wait for page to load finish
-    try:
-        delay = 5 #seconds
-        myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//table[@class="table-condensed"]')))
-    except TimeoutException:
-        print("page took too long to load")
+    crawl_individual_page(driver,web_url)
 
-    matches_link = [link.get_attribute("href") for link in driver.find_elements_by_xpath('//span[@class="event-list__event-name"]/a')]
+    # temp commented out
+    # wait_till_element_loads(driver,'//table[@class="table-condensed"]')
+
+    # matches_link = [link.get_attribute("href") for link in driver.find_elements_by_xpath('//span[@class="event-list__event-name"]/a')]
         
-    for link in matches_link:
-        crawl_individual_page(driver,link)
-        break
-
-    # matches_list = driver.find_elements_by_xpath('//table[@class="table-condensed"]/tbody/tr')
-    # for match in matches_list:
-    #     match.find_element_by_xpath('//td/span[@class="event-list__event-name"]')
+    # for link in matches_link:
+    #     crawl_individual_page(driver,link)
+    #     break
 
 
 # old code
